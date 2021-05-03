1f105794c3cd510b48d464bb018b1a70e11b8f94
jorgevz <jorgev2132@gmail.com>
2021-05-02 20:52:42 -0400

    moment.js installed

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 
-//this function only works in a 24 hour format.
-function timeCalculator (){
+// //this function only works in a 24 hour format.
+// function timeCalculator (){
 
-let hours = document.getElementById('hours').value*60
-let minutes = document.getElementById('minutes').value
-// let days = document.getElementById('days').value
+// let hours = document.getElementById('hours').value*60
+// let minutes = document.getElementById('minutes').value
+// // let days = document.getElementById('days').value
 
-//start time in minutes, we will use this to substract from arrival time later on.
-const startTime = 480;
-// formula to calculate minutes per hour and days.
-const timingCalculation = hours + + minutes - startTime;
+// //start time in minutes, we will use this to substract from arrival time later on.
+// const startTime = 480;
+// // formula to calculate minutes per hour and days.
+// const timingCalculation = hours + + minutes - startTime;
 
-// const arrivaltTime = timingCalculation*days
+// // const arrivaltTime = timingCalculation*days
 
-console.log(timingCalculation);
-
-
-document.getElementById('result').innerHTML = timingCalculation;
+// console.log(timingCalculation);
 
 
+// document.getElementById('result').innerHTML = timingCalculation;
 
-}
 
 
+// }
+
+var now  = "04/09/2013 15:00:00";
+var then = "02/09/2013 14:20:30";
+
+var ms = moment(now,"DD/MM/YYYY HH:mm:ss").diff(moment(then,"DD/MM/YYYY HH:mm:ss"));
+var d = moment.duration(ms);
+var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
 
